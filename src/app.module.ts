81c70e37827ea1c9007d9f1e5f3d51ef68969ca2
wuhaoayng <wb_wuhaoyang@meituan.com>
2023-05-11 20:36:13 +0800

    feat: 链接数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getConfig } from './utils';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './user/user.module'
 @Module({
   imports: [
     ConfigModule.forRoot({ ignoreEnvFile: true, isGlobal: true, load: [getConfig] }),
     UserModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql', // 数据库类型
+        entities: [],  // 数据表实体
+        host: configService.get('DB_HOST', 'localhost'), // 主机，默认为localhost
+        port: configService.get<number>('DB_PORT', 3306), // 端口号
+        username: configService.get('DB_USER', 'root'),   // 用户名
+        password: configService.get('DB_PASSWORD', 'whygxy981213'), // 密码
+        database: configService.get('DB_DATABASE', 'bookDB'), //数据库名
+        timezone: '+08:00', //服务器上配置的时区
+        synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
+      }),
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
